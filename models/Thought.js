db6a3fa8ec843c5fd323027f705c4321bfa60455
Hank Kapka <hankkapka@gmail.com>
2022-01-20 18:55:37 -0600

    updated thought model to include virtual

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,37 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema (
+    {
+        // set custom id to avoid confusion with parent comment _id
+        reactionId: {
+          type: Schema.Types.ObjectId,
+          default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+          type: String,
+          required: true,
+          trim: true
+        },
+        username: {
+          type: String,
+          required: true
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+          get: createdAtVal => dateFormat(createdAtVal)
+        }
+      },
+      {
+        toJSON: {
+          getters: true
+        }
+      }
+    
+);
+
+
 const ThoughtSchema = new Schema (
     {
         thoughtText: {
@@ -31,3 +62,13 @@ const ThoughtSchema = new Schema (
     }
 
 );
+
+// virtuals here
+ReactionSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+  });
+
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
