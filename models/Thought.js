0ad5d3e55ec4be6a8ede8bfbff66937d088d36c5
Hank Kapka <hankkapka@gmail.com>
2022-01-20 18:51:46 -0600

    updated thought model

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,33 @@
 const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema (
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            //must be 1-280 characters 
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        },
+        //username of who created the thought
+        username: {
+            type: String,
+            required: true
+          },
+
+          //use the reactions schema
+          reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        id: false
+    }
+
+);
